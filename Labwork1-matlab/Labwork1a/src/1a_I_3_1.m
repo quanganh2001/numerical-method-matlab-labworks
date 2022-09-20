5b4bb275a38e09a5ddbcd3c44e98153183a1a14d
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:34:10 +0700

    Write a function that calculates the mean of a vector. Application: vector x = 1:3:100

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_3_1.m
@@ -0,0 +1,12 @@
+﻿% ©2021 HP.
+% Write a function that calculates the mean of a vector.
+clc;
+clear;
+
+function [mean] = stats(x)
+    Number_of_elements = length(x)
+    mean = sum(x)/Number_of_elements;
+end
+
+x = [1:3:100]
+mean = stats(x)
