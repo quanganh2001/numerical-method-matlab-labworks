06d0a358b455bb1a43602b52fe87b7e7f3461884
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:41:53 +0700

    Doubling time of an investment: write a script to calculate hold long it will take for our bank investment to double. Suppose that the interest rate is 10% per year and all the interest is added to the initial investment.

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_6_3.m
@@ -0,0 +1,20 @@
+﻿% ©2021 HP.
+% While loop - Doubling time of an investment
+clc;
+clear;
+
+function time = InvestTime(InvestedMoney, Target, Rate)
+time = 0;
+while InvestedMoney < Target
+     time
+     InvestedMoney += InvestedMoney*Rate;
+     time += 1;
+end
+end
+
+InvestedMoney = 100
+Target = 2*InvestedMoney
+Rate = 0.1
+time = InvestTime(InvestedMoney, Target, Rate);
+fprintf('\nNumber of years needed to double the invested money: time = ');
+disp (time);
