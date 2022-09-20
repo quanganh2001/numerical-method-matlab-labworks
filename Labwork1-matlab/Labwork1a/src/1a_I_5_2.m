c2f6f71964c6dcac3d50c3efc9c1c44857ff07fa
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:39:49 +0700

    Write a script to calculate combination and permutation

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_5_2.m
@@ -0,0 +1,25 @@
+﻿% ©2021 HP.
+% For loop
+clc;
+clear;
+
+function Fac = Factorial(n)
+    Fac = 1;
+    for i=1:n
+        Fac *= i;
+    end
+end
+
+n = 6
+k = 3
+
+Fac_n = Factorial(n)
+
+Fac_k = Factorial(k)
+
+
+% a. Combination
+C_k_n = Factorial(n)/(Factorial(k)*Factorial(n-k))
+
+% b. Permutation
+A_k_n = Factorial(n)/(Factorial(n-k))
