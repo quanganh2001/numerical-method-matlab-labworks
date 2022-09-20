38a7d10e634966c2146e45492bc8c8fe73214bf4
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:43:22 +0700

    Add title, legend and labels for each axis.

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_7_1d.m
@@ -0,0 +1,17 @@
+﻿% ©2021 HP.
+clc;
+clear;
+clf;
+    
+hold on
+
+fplot(@(x) x+1, [-5 5], 'g-');
+fplot(@(x) -x+2, [-5 5], 'r--');
+
+title('Line Plot of y = x+1 and y = -x+2');
+xlabel('-5 < x < 5');
+ylabel('-5 < y < 5');
+legend({'y = x+1','y = -x+2'},'Location','northeast');
+grid on
+
+hold off
