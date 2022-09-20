4b890b89fe4390700af89caa453049237d9e3cd6
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:29:10 +0700

    Define a column vector x = (1, 2, 3, 4, 5); Transpose this vector?

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_1_2.m
@@ -0,0 +1,14 @@
+﻿% ©2021 HP.
+clc;
+clear;
+
+% Column vector x
+x_row=[1 2 3 4 5]
+x = [1;2;3;4;5];
+fprintf	('Column vector x = \n');
+disp(x);
+
+% Transpose vector x
+xT = x';
+fprintf	('\nTranspose of vector x: xT = \n');
+disp(xT);
