20da833a44a0a017b2ffa16fdfef905088924b7d
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:51:06 +0700

    Use either solve or fzero, as appropriate, to solve the following equations: 67x + 32 = 0 (exact solution)

---
+++ Labwork1-matlab/Labwork1b/src/1b_I_1_1a.m
@@ -0,0 +1,18 @@
+﻿% ©2021 HP.
+%Solve,fzero
+
+clc;
+clear;
+clf;
+
+%syms x
+%S = fsolve(67*x + 32 == 0, x)
+
+fnc = @(x) 67*x + 32
+S = fzero(y, 10)
+
+hold on
+fplot(y, 'r')
+plot(S, y(S), 'b+')
+grid on
+hold off
