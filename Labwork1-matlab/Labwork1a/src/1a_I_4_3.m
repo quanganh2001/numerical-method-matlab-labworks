e86aead4c8871063116fa87e3be46f66d7321305
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:36:25 +0700

    Write a script to calculate the price of electricity bill in Vietnam with input is the number of kWh

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_4_3.m
@@ -0,0 +1,34 @@
+﻿% ©2021 HP.
+% Electricity bill
+clc;
+clear;
+
+function price = Electricity(P)
+    price_50 = 50*1484;
+    price_100 = price_50 + (100-50)*1533;
+    price_200 = price_100 + (200-100)*1786;
+    price_300 = price_200 + (300-200)*2242;
+    price_400 = price_300 + (400-300)*2503;
+    if P<=50
+        price = P*1484;
+    elseif P<=100
+        price = price_50 + (P-50)*1533;
+    elseif P<=200
+        price = price_100 + (P-100)*1786;
+    elseif P<=300
+        price = price_200 + (P-200)*2242;
+    elseif P<=400
+        price = price_300 + (P-300)*2503;
+    else
+        price = price_400 + (P-400)*2587;
+    end
+end
+
+P = 200;
+price_200 = Electricity(P)
+
+
+P = 201;
+price_201 = Electricity(P)
+
+price_201 - price_200
