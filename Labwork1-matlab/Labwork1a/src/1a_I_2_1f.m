3b29e5f97eef731eabe5629d21bd99d41f6e3fed
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:33:35 +0700

    What’s the difference A(1:3) and A(1:3,1) or A(:,1)?

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_2_1f.m
@@ -0,0 +1,16 @@
+﻿% ©2021 HP.
+% Whatâ€™s the difference A(1:3) and A(1:3,1) or A(:,1)?
+clc;
+clear;
+
+A = zeros(3:3);
+A = diag([1 1 1], 0);
+
+A(2,1) = 2;
+A(3,1) = 3;
+A(3,2) = 4;
+A
+
+A1 = A(1:3)
+A2 = A(1:3,1)
+A3 = A(:, 1)
