529f37f384630d5838c42b4844b5359501131a18
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 09:24:31 +0700

    Use the function file above to Determine the positive real root of ln(x^2) = 0.7 using three iterations of the bisection method, with initial guesses of x_1 = 0.5 and x_u = 2

---
+++ Labwork1-matlab/Labwork1b/src/1b_II_1_2.m
@@ -0,0 +1,46 @@
+﻿% ©2021 HP.
+clc;
+clear;
+clf;
+
+func = @(x) log(x^2) - 0.7;
+
+xl = 0.5;
+xu = 2;
+es = 0.05;
+maxit = 100;
+
+[root,fx,ea,iter]=bisection(func,xl,xu,es,maxit);
+fprintf('- real root = %f\n', root);
+fprintf('- function value at root = %f\n', fx);
+fprintf('- approximate relative error (%%) = %f\n', ea);
+fprintf('- number of iterations = %d\n', iter);
+
+
+%--------------------------------------
+%GRAPHIC ANIMATION
+animate = 1;
+%--------------------------------------
+grid on
+hold on
+i = iter;
+fplot(func, [xl xu]);
+plot(root, 0, 'r+');
+leg = sprintf('Square root of x: (%f, 0)',root);
+xlabel('x');
+ylabel('y');
+legend({'log(x^2) - 0.7',leg},'Location','northeast');
+%--------------------------------------
+RootValue = [];
+if animate == 1
+    for maxit=1:i
+        [root,fx,ea,iter]=bisection(func,xl,xu,es,maxit);
+        RootValue = [RootValue;root];
+    end
+    for maxit=1:i
+        fprintf('Root value = %f', RootValue(maxit));
+        plot(RootValue(maxit), 0);
+        pause(0.5);
+    end
+end
+hold off
