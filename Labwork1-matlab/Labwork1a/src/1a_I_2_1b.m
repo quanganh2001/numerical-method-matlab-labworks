34d6394ecacfcf337377298e6ae9cb55fd9281fb
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:31:39 +0700

    Change the diagonal of A to 1 without define the matrix again using diag

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_2_1b.m
@@ -0,0 +1,12 @@
+﻿% ©2021 HP.
+% Change the diagonal of A to 1 without define the matrix again
+%help diag
+clc;
+clear;
+
+A = zeros(3:3)
+
+A(1,1) = 1;
+A(2,2) = 1;
+A(3,3) = 1
+%A = diag([1 1 1], 0)
