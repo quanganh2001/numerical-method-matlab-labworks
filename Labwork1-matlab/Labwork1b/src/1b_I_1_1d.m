edf2e3520520e7acfe1da821b19dc87a56f49a86
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:52:47 +0700

    Use either solve or fzero, as appropriate, to solve the following equations: e^x = 8x - 4 (all real solutions). It help to draw a picture first.

---
+++ Labwork1-matlab/Labwork1b/src/1b_I_1_1d.m
@@ -0,0 +1,22 @@
+﻿% ©2021 HP.
+%Solve,fzero
+
+clc;
+clear;
+clf;
+
+%syms x
+%S = solve(exp(x) == 8*x - 4, x)
+
+fnc = @(x) exp(x) - 8*x + 4;
+S = fzero(y, 1)
+
+%------------------
+hold on
+
+fplot(@(x) exp(x), [-1 2], 'b');
+fplot(@(x) 8*x - 4, [-1 2], 'r');
+plot(S ,exp(S), 'ro')
+grid on
+
+hold off
