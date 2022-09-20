d1929d24809331033d64b60c346015123e911527
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 09:13:17 +0700

    Use the function file above to find the square root of 2, taking 1 and 2 as initial values of x_i and x_u. Continue bisecting until the maximum error is less than 0.05

---
+++ Labwork1-matlab/Labwork1b/src/1b_II_1_1.m
@@ -0,0 +1,46 @@
+﻿% ©2021 HP.
+clc;
+clear;
+clf;
+
+
+func = @(x) x^2-2;
+
+xl = 1;
+xu = 2;
+es = 0.05;
+maxit = 100;
+[root,fx,ea,iter]=bisection(func,xl,xu,es,maxit);
+
+fprintf('- real root = %f\n', root);
+fprintf('- function value at root = %f\n', fx);
+fprintf('- approximate relative error (%%) = %f\n', ea);
+fprintf('- number of iterations = %d\n', iter);
+
+
+%--------------------------------------
+%GRAPHIC ANIMATION
+animate = 1;
+%--------------------------------------
+grid on
+hold on
+fplot(func, [xl xu]);
+plot(root, 0, 'r+');
+leg = sprintf('Square root of x: (%f, 0)',root);
+xlabel('x');
+ylabel('y');
+legend({'y = x^2-2',leg},'Location','northeast');
+%--------------------------------------
+RootValue = [];
+if animate == 1
+    for maxit=1:11
+        [root,fx,ea,iter]=bisection(func,xl,xu,es,maxit);
+        RootValue = [RootValue;root];
+    end
+    for maxit=1:11
+        fprintf('Root value = %f', RootValue(maxit));
+        plot(RootValue(maxit), 0);
+        pause(0.5);
+    end
+end
+hold off
