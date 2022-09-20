464a7cc88b55cba4bdef8734388562f2cd1ac291
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 09:44:04 +0700

    Determine the positive root of f(x) = x^{10} - 1 using the Newton Raphson method and an initial guess of x = 0.5

---
+++ Labwork1-matlab/Labwork1b/src/1b_II_2_2.m
@@ -0,0 +1,59 @@
+﻿% (c)2021 HP.
+clc;
+clear;
+clf;
+
+f = @(x) x^10 - 1;
+df = @(x) 9*x^9;
+%--------------------------------------
+x0 = 0.5;
+step = 40;
+e0 = 10^(-8);
+%--------------------------------------
+xk = x0;
+xkValue = [];
+err = x0;
+i = 0;
+while err>=e0
+    fprintf('\nStep = %d\n',i);
+    xk1 = xk-f(xk)/df(xk);
+    fprintf('x(k+1) = %f\n',xk1);
+    fprintf('f(x(k+1)) = %f\n',f(xk1));
+    err1 = abs((xk-xk1)/xk);
+    err = err1;
+    fprintf('err = abs((xk-xk1)/xk) = %e\n', err);
+    xkValue = [xkValue; xk1 f(xk1)];
+    xk = xk1;
+    i+=1;
+end
+
+xkValue
+maxit = length(xkValue)
+%--------------------------------------
+%GRAPHIC ANIMATION
+animate = 1;
+%--------------------------------------
+grid on
+hold on
+scale = 0.1;
+xl = xk-xk*scale;
+xu = xk+xk*scale;
+fplot(f, [xl xu]);
+%fplot(df, [xl xu]);
+plot(xk, 0, 'r+');
+leg = sprintf('x0: (%f, 0)',xk);
+xlabel('x');
+ylabel('y');
+legend({'f = @(x) x^10 - 1',leg},'Location','northeast');
+%--------------------------------------
+%disp(xkValue);
+if animate == 1
+    for it=1:maxit
+        if xkValue(it,2)<=xkValue(it,1)
+            fprintf('%d: [xk f(xk)] = [%f %f]', it, xkValue(it,1), xkValue(it,2));
+            plot(xkValue(it,1),xkValue(it,2));
+            pause(0.5);
+        end
+    end
+end
+hold off
