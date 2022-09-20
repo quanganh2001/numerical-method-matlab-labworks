8f5dcb73bf5c58e3d75a04bc5cba30a6f94b9d1f
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:35:27 +0700

    Write brief scripts to evaluate the following functions

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_4_1.m
@@ -0,0 +1,15 @@
+﻿% ©2021 HP.
+%Write brief scripts to evaluate the following functions. 
+% m = n + 1 with n >1
+% m = n - 1 with n <1
+clc;
+clear;
+
+n = 5
+%n = -5
+
+if n>1
+    m = n+1
+else
+    m = n-1
+end
