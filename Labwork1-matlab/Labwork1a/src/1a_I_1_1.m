8d7e3d2f144c02b884ec3198d057af0ae18d7d1b
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:28:09 +0700

    Define a row vector x, one row, five column with value = (1, 2, 3, 4, 5); find the value x(3) + x(2)

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_1_1.m
@@ -0,0 +1,13 @@
+﻿% ©2021 HP.
+clc;
+clear;
+
+%Define a row vector x
+x=[1 2 3 4 5];
+fprintf	('Row vector x = \n');
+disp(x);
+
+%find the value x(3)+x(2)
+x32 = x(3)+x(2);
+fprintf	('\nx32 = x(3)+x(2) = ');
+disp(x32);
