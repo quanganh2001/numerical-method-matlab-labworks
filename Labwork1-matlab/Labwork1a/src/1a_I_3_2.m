c24aa5f9c52161d50e8e78e63a3f017ff103ef37
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:34:55 +0700

    Write a function that solves the quadratic equation ax^2 + bx + c = 0. Application: x^2 + 2x - 3 = 0

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_3_2.m
@@ -0,0 +1,15 @@
+﻿% ©2021 HP.
+% Write a function that solves the quadratic equation ax^2 + bx + c = 0
+clc;
+clear;
+
+function [x1 x2] = quadratic(a, b, c)
+    D = b^2-4*a*c;
+    x1 = (-b - sqrt(D))/(2*a);
+    x2 = (-b + sqrt(D))/(2*a);
+end
+    
+a = 1;
+b = 2;
+c = -3;
+[x1 x2] = quadratic(a, b, c)
