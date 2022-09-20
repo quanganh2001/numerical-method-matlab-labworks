4e1ab28de207660a2685920f6a66995f8ade12b8
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:31:16 +0700

    Define element by element and used the function zeros

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_2_1a.m
@@ -0,0 +1,16 @@
+﻿% ©2021 HP.
+% Define matrix A by two approaches
+clc;
+clear;
+
+% Method 1: Define element by element
+A1 = [0 0 0;0 0 0;0 0 0];
+% Showing the result
+fprintf('\nA1 = \n');
+disp(A1);
+
+% Method 2: the function zeros
+A2 = zeros(3:3);
+% Showing the result
+fprintf('\nA2 = \n');
+disp(A2);
