7c5297b297e87045044fd34b8da1701e6dc0c624
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:57:54 +0700

    Write your script file to implement the Bisection method for solving the equation: x^2 + 2x - 3 = 0

---
+++ Labwork1-matlab/Labwork1b/src/1b_I_2_1c.m
@@ -0,0 +1,14 @@
+﻿% ©2021 HP.
+clc;
+clear;
+clf;
+
+hold on
+
+%fplot(@(x) e^(-x/2), [-1.5 1.5]);
+%fplot(@(x) x^4 - x^2, [-1.5 1.5]);
+
+fplot(@(x) e^(-x/2), [-1.5 1.5]);
+fplot(@(x) x^4 - x^2, [-1.5 1.5]);
+grid on
+hold off
