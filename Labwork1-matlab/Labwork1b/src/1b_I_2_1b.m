82599d717a1cb9a6c0acb1cc709e25254d5fb456
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:56:17 +0700

    Use the function file above to Determine the positive real root of ln(x^2) = 0.7 using three iterations of the bisection method, with initial guesses of x_1 = 0.5 and x_u = 2.

---
+++ Labwork1-matlab/Labwork1b/src/1b_I_2_1b.m
@@ -0,0 +1,16 @@
+﻿% ©2021 HP.
+clc;
+clear;
+clf;
+
+%fplot(@(x) tan(x/2), [-pi pi -10 10]);
+
+fplot(@(x) tan(x/2), [-pi pi -10 10]) ;
+
+%x =linspace(-10, 10,1000)
+%y = tan(x/2)
+%plot(x,y)
+%xlim([-pi pi])
+%ylim([-10 10])
+
+grid on
