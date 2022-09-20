244c771d371568fce3a876c9701b3573980a19c3
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:32:38 +0700

    Calculate A+B, A-B, A*B and A.*B. Give remark about the results of A*B and A.*B

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_2_1d.m
@@ -0,0 +1,25 @@
+﻿% ©2021 HP.
+% Calculate A+B, A-B, A*B and A.*B
+clc;
+clear;
+
+A = zeros(3:3);
+
+A = diag([1 1 1], 0);
+
+A(2,1) = 2;
+A(3,1) = 3;
+A(3,2) = 4;
+
+A
+
+B = A'
+
+fprintf('\nA+B = \n');
+disp(A+B);
+fprintf('\nA-B = \n');
+disp(A-B);
+fprintf('\nA*B = \n');
+disp(A*B);
+fprintf('\nA.*B = \n');
+disp(A.*B);
