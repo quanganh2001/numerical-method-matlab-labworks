bcf54db36a0036baa370198d7d56811d64ec0a71
quanganh2001 <cocoi5659@gmail.com>
2022-09-20 08:35:54 +0700

    Write brief scripts to evaluate the following functions. (If else structure)

---
+++ Labwork1-matlab/Labwork1a/src/1a_I_4_2.m
@@ -0,0 +1,22 @@
+﻿% ©2021 HP.
+% Write brief scripts to evaluate the following functions. 
+% y=4x with 0<x <10
+% y=10x with 10<x<40
+% y = x with x > 40 and x < 0
+
+clc;
+clear;
+
+
+%x = 5
+%x = -5
+x = 20
+
+
+if (0<x & x<10)
+    y = 4*x
+elseif (10<x & x<40)
+    y = 10*x
+else
+    y = x
+endif
